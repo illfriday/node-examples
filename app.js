8e048afa99639a214f20e3503a3e12b8f11ec72f
illfriday <willprouty@gmail.com>
2021-09-28 12:04:53 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const rect = {
+  perimeter: (x, y) => 2 * (x + y),
+  area: (x, y) => x * y,
+};
+
+function solveRect(l, w) {
+  console.log(`Solving for rectangle with dimensions of: ${l}, ${w}`);
+
+  if (l <= 0 || w <= 0) {
+    console.log(
+      `Rectangle dimensions must be greater than zero. Received: ${l}, ${w}`
+    );
+  } else {
+    console.log(`Area of rectangle: ${rect.area(l, w)}`);
+    console.log(`Perimeter of rectangle: ${rect.perimeter(l, w)}`);
+  }
+}
+//valid
+solveRect(2, 4);
+solveRect(3, 5);
+//invalid
+solveRect(0, 5);
+solveRect(2, -4);
