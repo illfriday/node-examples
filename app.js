6532988964e561352a2280da125c28c89005fab6
illfriday <willprouty@gmail.com>
2022-03-29 16:25:23 -0700

    add asynchronous function to rectangle.js, move error handling to rectangle.js.

--- app.js
+++ app.js
@@ -10,15 +10,20 @@ const rect = require("./rectangle");
 function solveRect(l, w) {
   console.log(`Solving for rectangle with the dimension ${l} & ${w}`);
 
-  if (l <= 0 || w <= 0) {
-    console.log(
-      `Error. Rectangle dimensions must be greater than 0. Received: ${l}, ${w}.`
-    );
-  } else {
-    console.log(`Area of rectangle: ${rect.area(l, w)}.`);
+  rect(l, w, (err, rectangle) => {
+    if (err) {
+      console.log("ERROR", err.message);
+    } else {
+      console.log(
+        `Area of rectangle with dimensions ${l}, ${w}: ${rectangle.area()}.`
+      );
 
-    console.log(`Perimeter of rectangle: ${rect.perimeter(l, w)}.`);
-  }
+      console.log(
+        `Perimeter of rectangle with dimensions ${l}, ${w}: ${rectangle.perimeter()}.`
+      );
+    }
+  });
+  console.log("This statement is logged after the call to rect.()");
 }
 
 solveRect(0, 9);
--- rectangle.js
+++ rectangle.js
@@ -1,2 +1,18 @@
-exports.perimeter = (x, y) => 2 * (x + y);
-exports.area = (x, y) => x * y;
+module.exports = (x, y, callback) => {
+  if (x <= 0 || y <= 0) {
+    callback(
+      new Error(
+        `Error. Rectangle dimensions must be greater than 0. Received: ${x}, ${y}.`
+      )
+    );
+  } else {
+    setTimeout(
+      () =>
+        callback(null, {
+          perimeter: () => 2 * (x + y),
+          area: () => x * y,
+        }),
+      2000
+    );
+  }
+};
